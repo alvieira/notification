e47a1057209bcf740bcfdd890afcb7428eeb6ace
Aloisio Eduardo Vieira <alvieira@br.ibm.com>
2018-08-07 11:17:24 -0300

    entities generated using JDL

---
+++ src/main/java/com/mycompany/store/repository/NotificationRepository.java
@@ -0,0 +1,14 @@
+package com.mycompany.store.repository;
+
+import com.mycompany.store.domain.Notification;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data MongoDB repository for the Notification entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+
+}
